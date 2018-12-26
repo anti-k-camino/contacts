68c8f5f7b38bbe5936f833ec80aaac81ce420fef
antonoff <kratyuk.anton84@gmail.com>
2018-12-26 20:26:46 +0200

    display contactas, remove option

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import ListContacts from './ListContacts'
 
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+     {
+       "id": "karen",
+       "name": "Karen Isgrigg",
+       "handle": "karen_isgrigg",
+       "avatarURL": "http://localhost:5001/karen.jpg"
+     },
+     {
+       "id": "richard",
+       "name": "Richard Kalehoff",
+       "handle": "richardkalehoff",
+       "avatarURL": "http://localhost:5001/richard.jpg"
+     },
+     {
+       "id": "tyler",
+       "name": "Tyler McGinnis",
+       "handle": "tylermcginnis",
+       "avatarURL": "http://localhost:5001/tyler.jpg"
+     }
+    ]
+  }
+
+  removeContact(contact) {
+    this.setState((current) => ({
+      contacts: current.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+  }
+
+  removeContact = this.removeContact.bind(this)
+
   render() {
     return (
-      <div>
-        <ListContacts contacts={contacts} />
-      </div>
+      <Fragment>
+        <ListContacts
+          contacts={this.state.contacts}
+          onDelete={this.removeContact}
+        />
+      </Fragment>
     )
   }
 }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react'
+import React from "react"
+import PropTypes from "prop-types"
 
-class ListContacts extends Component {
-  render() {
-    console.log('Props ', this.props)
-    return (
-      <ol className='contact-list'>
-      </ol>
-    )
-  }
+function ListContacts(props) {
+  return (
+    <ol className="contact-list">
+      { props.contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{backgroundImage: `url(${contact.avatarURL})`}}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>@{contact.handle}</p>
+            </div>
+            <button
+              className="contact-remove"
+              onClick={(e) => props.onDelete(contact)}
+            >
+              Remove
+            </button>
+          </li>
+        ))
+      }
+    </ol>
+  )
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired
 }
 
 export default ListContacts
